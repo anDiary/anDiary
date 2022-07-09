0745773812ddffa0d26f1f77fd31fd4dca7cc5a5
Naffy Dharni <naffydharni006@gmail.com>
2022-07-09 23:19:56 +0530

    update: version display on homepage

--- apps/app/preload.js
+++ apps/app/preload.js
@@ -5,14 +5,3 @@ contextBridge.exposeInMainWorld('versions', {
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
 })
-
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
-
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
-})
